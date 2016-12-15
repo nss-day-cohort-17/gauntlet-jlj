1ef1fc4092baceca28500bcbc221a1750406d1ad
wierje <wier.je@gmail.com>
2016-12-15 15:48:53 -0600

    added images and events.js

---
+++ javascripts/events.js
@@ -0,0 +1,40 @@
+
+$("#bomb").click(function() {
+  console.log("bomb clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/bomb-154456_640.png'));
+});
+
+$("#dagger").click(function() {
+  console.log("dagger clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/dagger.png'));
+});
+
+$("#broadsword").click(function() {
+  console.log("broadsword clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/sword-308836_640.png'));
+});
+
+$("#waraxe").click(function() {
+  console.log("waraxe clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/axe-47042_640.png'));
+});
+
+$("#mace").click(function() {
+  console.log("mace clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/mace.jpg'));
+});
+
+$("#lance").click(function() {
+  console.log("lance clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/lance.png'));
+});
+
+$("#pike").click(function() {
+  console.log("pike clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/pike.png'));
+});
+
+$("#tomahawk").click(function() {
+  console.log("tomahawk clicked");
+  $("#showWeapon").html($("<img>").attr("src",'../img/tomahawk.png'));
+});
